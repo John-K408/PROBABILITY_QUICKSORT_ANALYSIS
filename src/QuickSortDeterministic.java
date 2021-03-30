1ee8b0652cfc3251c86b7e6bbd7dc96f97369be5
John-K408 <jkoomson23@amherst.edu>
2021-03-30 14:11:07 -0400

    Implemented step 01 and 02 for the project --- we just have to get data being printed to console and pllot

--- src/QuickSortDeterministic.java
+++ src/QuickSortDeterministic.java
@@ -5,15 +5,26 @@ public class QuickSortDeterministic {
         int[] arr2 = arr.generateMostlySortedInput(10);
         int[] arr3 = arr.generateMostlySortedInput(10);*/
 
-        int[] A = {1,0,5};
+        String[] arrayTypes = new String[]{"generateRandomInput","generatePartiallySortedInput","generateMostlySortedInput"};
 
-        System.out.println("----comparison count------");
-        System.out.println(QuickSort(A,0, A.length-1,0));
+//        double average = computeAverage(20,100000,arrayTypes[0]);
 
-        for (int j : A) {
-            System.out.println("----sorted elements------");
-            System.out.println(j);
+        int maxArraySize = 1000000;
+
+        for(int n = 1; n <= maxArraySize;n++){
+            System.out.println("ArraySize "+ n + ": " + computeAverage(n,100000,arrayTypes[0]));
         }
+
+
+//        int[] A = {1,4,3,8,7,5,9,0};
+//
+//        System.out.println("----comparison count------");
+//        System.out.println(QuickSort(A,0, A.length-1,0));
+//
+//        for (int j : A) {
+//            System.out.println("----sorted elements------");
+//            System.out.println(j);
+//        }
 
 
     }
@@ -54,5 +65,52 @@ public class QuickSortDeterministic {
         array[i] = array[j];
         array[j] = temp;
     }
+
+    public static double computeAverage(int arraysize, int numTries, String arrayInputType){
+        //if the user puts in the wrong arraysize to be generated, print error
+        if(arraysize <1) System.err.println("The size of the array must be at least 1");
+
+        //initialize array --- doesn't really do anything.
+        int[] array = new int[1];
+        Generate generator = new Generate();
+
+        //since average is a sum divided by number of times, we will sum all the counts on the different inputs
+        //of the same size and divide by the number of times we run quicksort.
+
+        double totalCounter = 0;
+
+        for(int i = 0; i < numTries;i++){
+
+            if(arrayInputType.toLowerCase().equals("generaterandominput")){
+                array = generator.generateRandomInput(arraysize);
+            }
+            else if(arrayInputType.toLowerCase().equals("generatepartiallysortedinput")){
+                array = generator.generatePartiallySortedInput(arraysize);
+            }
+
+            else if(arrayInputType.toLowerCase().equals("generatemostlysortedinput")){
+                array = generator.generateMostlySortedInput(arraysize);
+            }
+            else{
+                System.err.println("Please input correct input type for the array");
+            }
+
+            //the count (number of comparisons) of quicksort for this current input
+            int currCount = QuickSort(array,0,arraysize - 1,0);
+
+//            System.out.println("-------Current count-----");
+//            System.out.println(currCount);
+
+            totalCounter += currCount;
+        }
+
+//        System.out.println("-----------Average-------------");
+//        System.out.println(totalCounter/numTries);
+
+
+        return totalCounter/numTries;
+
+    }
+
 
 }
